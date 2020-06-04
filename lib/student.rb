8a7ed3fcffdccd3589398528897988e7bac9f075
michael thomas <michael.thomas2014@gmail.com>
2020-06-04 17:21:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   
   def self.create_table
     sql = <<- SQL
-    CREATE TABLE students VALUES (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER); 
+    CREATE TABLE IF NOT EXISTS students VALUES (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER); 
     SQL
     
   end
